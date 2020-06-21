8e2d51680d500a946785a9712e468424076ec1f2
Lukia <ac.lukia.berthet@gmail.com>
2020-06-21 15:37:22 +0200

    Fix color reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,27 @@
 
 #include <BluetoothSerial.h> //Header File for Serial Bluetooth, will be added by default into Arduino
 
-static byte RED_PIN = 25;
-static byte GREEN_PIN = 26;
-static byte BLUE_PIN = 27;
+static const byte RED_PIN = 13;
+static const byte GREEN_PIN = 12;
+static const byte BLUE_PIN = 14;
 
-static int RED_CHANNEL = 0;
-static int GREEN_CHANNEL = 1;
-static int BLUE_CHANNEL = 2;
+static const int RED_CHANNEL = 0;
+static const int GREEN_CHANNEL = 1;
+static const int BLUE_CHANNEL = 2;
 
-static int PWM_FREQ = 5000;
-static int PWM_RES = 8;
-
-static int idx = 0;
+static const int PWM_FREQ = 5000;
+static const int PWM_RES = 8;
 
 BluetoothSerial btSerial; //Object for Bluetooth
-int incoming;
+
+byte color[3];
+int count = 0;
+int data;
 
 void setup()
 {
   Serial.begin(9600);
-  
+
   // configure LED PWM functionalitites
   ledcSetup(RED_CHANNEL, PWM_FREQ, PWM_RES);
   ledcSetup(GREEN_CHANNEL, PWM_FREQ, PWM_RES);
@@ -44,22 +45,23 @@ void loop()
 
   if (btSerial.available()) //Check if we receive anything from Bluetooth
   {
-    incoming = btSerial.read(); //Read what we recevive
-    Serial.print("Received:");
-    Serial.println(incoming);
+    data = btSerial.read(); //Read what we recevive
 
-    if (incoming == 49)
+    color[count] = (byte)(data & 0xFF);
+
+    if (count == 2)
     {
-      digitalWrite(LED_BUILTIN, HIGH);
-      btSerial.println("LED turned ON");
+      ledcWrite(RED_CHANNEL, color[0]);
+      ledcWrite(GREEN_CHANNEL, color[1]);
+      ledcWrite(BLUE_CHANNEL, color[2]);
+
+      Serial.println("Color:" + String(color[0]) + ", " + String(color[1]) + ", " + String(color[2]));
+      count = 0;
     }
-
-    if (incoming == 48)
+    else
     {
-      digitalWrite(LED_BUILTIN, LOW);
-      btSerial.println("LED turned OFF");
+      count++;
     }
   }
-  
-   delay(20);
+  delay(10);
 }
